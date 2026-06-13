255a0fd871cc19009ac9c54f6a57d0a5f32922dd
RiyaJanday <riyajanday1677@gmail.com>
2026-06-13 21:58:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const cards = document.querySelectorAll(".box1, .box2, .box3, .box4, .box5");
+
+cards.forEach(function (card, index) {
+
+    if (index !== 0) {
+        gsap.from(card, {
+            y: 400,
+            scrollTrigger: {
+                trigger: card,
+                start: "top 85%",
+                end: "top 30%",
+                scrub: 1,
+            }
+        });
+    }
+});
+
+gsap.to(".page2 .textWrapper", {
+    marginLeft: "-300%",
+    delay: 1,
+    ease: 2,
+    scrollTrigger: {
+        trigger: ".page2",
+        start: "top top",
+        scrub: true,
+        pin: true
+    }
+});
